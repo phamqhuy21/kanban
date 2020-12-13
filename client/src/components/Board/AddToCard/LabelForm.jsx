2ae6a307eb057e52e624b3769ffa3386b7a0989f
huyy <phamqhuy21>
2020-12-13 12:55:00 +0700

    fix(LabelForm.jsx): update style, ui

--- client/src/components/Board/AddToCard/LabelForm.jsx
+++ client/src/components/Board/AddToCard/LabelForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Form, Input, Card } from "antd";
-import { CheckOutlined } from "@ant-design/icons";
+import { Form, Input, Card, Row, Col } from "antd";
+import { CheckOutlined, EditOutlined } from "@ant-design/icons";
 import PropTypes from "prop-types";
 
 LabelForm.propTypes = {
@@ -28,40 +28,52 @@ function LabelForm(props) {
 
   return (
     <Card
-      title={<div>Nhãn</div>}
+      size="small"
+      title={<div style={{ textAlign: "center" }}>Nhãn</div>}
       bordered={false}
-      bodyStyle={{ padding: "0" }}
-      headStyle={{ padding: "0" }}
+      //   bodyStyle={{ padding: "0" }}
+      //   headStyle={{ padding: "0" }}
     >
-      <Form style={{ width: "20vw" }} form={form}>
-        <Form.Item name="search">
-          <Input placeholder="Tìm nhãn..." onChange={handleSearch} />
+      <Form style={{ width: "17vw" }} form={form}>
+        <Form.Item name="search" style={{ marginBottom: "1vh" }}>
+          <Input
+            placeholder="Tìm nhãn..."
+            onChange={handleSearch}
+            style={{ border: "3px solid rgba(100,100,100,0.2)" }}
+          />
         </Form.Item>
         <Form.Item>
           <div>
-            <h3>Nhãn</h3>
+            <p style={{ fontWeight: "500" }}>Nhãn</p>
             {label.map((color, index) => {
               return (
-                <Card
-                  size="small"
-                  key={index}
-                  extra={
-                    card.label.includes(color) ? (
-                      <CheckOutlined style={{ color: "white" }} />
-                    ) : null
-                  }
-                  style={{
-                    width: "90%",
-                    height: "5vh",
-                    borderRadius: "0.2rem",
-                    margin: "1vh",
-                    backgroundColor: `${color}`,
-                    cursor: "pointer",
-                  }}
-                  onClick={() => {
-                    selectLabel(color, card);
-                  }}
-                />
+                <Row style={{ marginBottom: "0.5vh" }}>
+                  <Col span={22}>
+                    <Card
+                      size="small"
+                      key={index}
+                      extra={
+                        card.label.includes(color) ? (
+                          <CheckOutlined style={{ color: "white" }} />
+                        ) : null
+                      }
+                      style={{
+                        borderRadius: "0.2rem",
+                        backgroundColor: `${color}`,
+                        cursor: "pointer",
+                      }}
+                      bodyStyle={{ padding: "0" }}
+                      title="as"
+                      headStyle={{ padding: "0 1vw" }}
+                      onClick={() => {
+                        selectLabel(color, card);
+                      }}
+                    />
+                  </Col>
+                  <Col span={2}>
+                    <EditOutlined style={{ marginLeft: "0.5vw" }} />
+                  </Col>
+                </Row>
               );
             })}
           </div>
